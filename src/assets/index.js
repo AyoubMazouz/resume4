bb5d0536be43acc409a74c902727ddc66bd7d1b1
AyoubMazouz <ayoub2000mazouz@gmail.com>
2024-01-11 19:15:27 +0100

    init

---
+++ src/assets/index.js
@@ -0,0 +1,46 @@
+import logo from "/logo.png"
+import backend from "./backend.png"
+import creator from "./creator.png"
+import mobile from "./mobile.png"
+import web from "./web.png"
+import github from "./github.png"
+import menu from "./menu.svg"
+import close from "./close.svg"
+
+import python from "./tech/python.png"
+import docker from "./tech/docker.png"
+import git from "./tech/git.png"
+import javascript from "./tech/javascript.png"
+import reactjs from "./tech/reactjs.png"
+import tailwind from "./tech/tailwind.png"
+import typescript from "./tech/typescript.png"
+import laravel from "./tech/laravel.png"
+import go from "./tech/go.png"
+import java from "./tech/java.png"
+import php from "./tech/php.png"
+import cSharp from "./tech/cSharp.png"
+import mysql from "./tech/mysql.png"
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  docker,
+  git,
+  javascript,
+  laravel,
+  java,
+  php,
+  cSharp,
+  mysql,
+  go,
+  python,
+  reactjs,
+  tailwind,
+  typescript,
+}
