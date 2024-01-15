5a3d65dcb3aca7e22dd773e3f2e4d5978c67cd78
AyoubMazouz <ayoub2000mazouz@gmail.com>
2024-01-15 13:36:36 +0100

    remove unused libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { BrowserRouter } from "react-router-dom"
 import { About, Contact, Experience, Hero, Navbar, Tech, Projects, StarsCanvas } from "./components"
 import ProjectModel from "./components/ProjectModel"
 import { GlobalContextProvider } from "./contexts/globalContext"
@@ -7,23 +6,21 @@ import { GlobalContextProvider } from "./contexts/globalContext"
 const App = () => {
   return (
     <GlobalContextProvider>
-      <BrowserRouter>
-        <ProjectModel />
-        <div className="relative z-0 bg-primary">
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar />
-            <Hero />
-          </div>
-          <About />
-          <Experience />
-          <Tech />
-          <Projects />
-          <div className="relative z-0">
-            <Contact />
-            <StarsCanvas />
-          </div>
+      <ProjectModel />
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
         </div>
-      </BrowserRouter>
+        <About />
+        <Experience />
+        <Tech />
+        <Projects />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
     </GlobalContextProvider>
   )
 }
