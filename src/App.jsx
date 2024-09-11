939562fd2a23a85076e4ce5a09343b072eff45a3
Ayoub Mazouz <ayoub2000mazouz@gmail.com>
2024-09-11 21:32:21 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { Icon } from "@iconify/react"
 
 const Container = ({ title, children }) => {
   return (
-    <div className={`relative px-4 mt-2`}>
+    <div className={`relative px-4 mt-6`}>
       <div className={`px-4 text-lg border-t border-b border-primary uppercase text-primary font-bold`}>{title}</div>
-      <div className="mt-2">{children}</div>
+      <div className="mt-3">{children}</div>
     </div>
   )
 }
 const App = () => {
   return (
-    <div className="py-2 space-y-3 print:space-y-0 print:py-0 text-dark text-xs font-dm leading-[.95rem] bg-gray-900">
+    <div className="py-3 space-y-5 print:space-y-0 print:py-0 text-dark font-dm leading-[1.3] bg-gray-900">
       {/* <CoverLetter /> */}
 
       <div className="w-[210mm] h-[297mm] mx-auto bg-light rounded-xl overflow-hidden">
@@ -32,24 +32,20 @@ const App = () => {
                 <Icon icon="carbon:phone" width="22px" />
                 {details[0]}
               </div>
-              <div className={`col-span-6 flex items-center gap-x-2 bg-light rounded px-2 py-1 text-primary`}>
+              <div className={`col-span-full flex items-center gap-x-2 bg-light rounded px-2 py-1 text-primary`}>
                 <Icon icon="carbon:phone" width="22px" />
-                {details[2]}
-              </div>
-              <div className={`col-span-6 flex items-center gap-x-2 bg-light rounded px-2 py-1 text-primary`}>
-                <Icon icon="carbon:email" width="22px" />
                 {details[1]}
               </div>
               <div className={`col-span-full flex items-center gap-x-2 bg-light rounded px-2 py-1 text-primary`}>
                 <Icon icon="carbon:location-person" width="22px" />
-                {details[3]}
+                {details[2]}
               </div>
             </div>
           </div>
         </div>
         {/* Formation */}
         <Container title="Formation">
-          <div className="space-y-2">
+          <div className="space-y-4">
             {education?.map((education) => (
               <div key={education[1]} className="grid grid-cols-12">
                 <span className="col-span-3">{education[0]}</span>
@@ -60,7 +56,7 @@ const App = () => {
         </Container>
         {/* Experience */}
         <Container title="Expérience">
-          <div className="space-y-2">
+          <div className="space-y-4">
             {experiences?.map((experience) => (
               <div key={experience[1]} className="grid grid-cols-12">
                 <span className="col-span-3">{experience[0]}</span>
--- src/constants/fr.jsx
+++ src/constants/fr.jsx
@@ -1,116 +1,28 @@
-export const fullName = ["Aymane", "ABBA"]
-export const title = "Consultant technique en diagnostic automobile"
+export const fullName = ["Ilyass", "Zerraki"]
+export const title = "Technicien Automobile"
 export const details = [
-  "29 ans, célibataire",
-  "abbaaymane@gmail.com",
-  "+212 6572-20716",
-  "Hay El Omaria Rue 1 IMM 160   Apt 14 AIN CHOCK CASABLANCA",
+  "ilyaszerraki@gmail.com",
+  "+212 688 551161",
+  "Bachkou Sidimaarouf Imm 90   Apt 1 AIN CHOCK CASABLANCA",
 ]
 
 export const education = [
-  [
-    "2015 - 2017 ",
-    <>
-      Diplôme de technicien spécialisé en diagnostic et électronique embarquée au centre de recherche et d´engineering
-      automobile
-    </>,
-  ],
-  ["2017", <>Participations au WORLD SKILLS</>],
-  [
-    "2013 - 2015",
-    <>Diplôme de technicien en réparation des engins à moteur au centre de recherche et d´engineering automobile</>,
-  ],
+  ["2023 - 2022 ", <>Niveau bac en sciences de la vie et de la terre</>],
+  ["2024 - 2026", <>Diplôme de technicien en diagnostic Et Électronique Embarquée au centre ISTA Lissasfa (OFPPT)</>],
 ]
 
-export const experiences = [
-  [
-    "Juin 2024 - Poste actuel",
-    <>
-      <h1 className="font-semibold">Consultant support technique/Relai du périmètre GDEP (PSA)</h1>
-      <h1 className="font-semibold">Support technique des TEC auprès de STELLANTIS (PSA FCA)</h1>
-      <ul className="list-disc list-inside">
-        <li> Analyse des données d'entrées et des demandes d'évolution.</li>
-        <li>Motorisation diesel et chaîne de transmission DEEP.</li>
-        <li>Télématique et véhicule connecté TMCV.</li>
-        <li>Systèmes châssis CHBO.</li>
-        <li>Motorisation essence GEGH.</li>
-        <li>ADAS.</li>
-      </ul>
-    </>,
-  ],
-  [
-    "Juin 2023 - Juin 2024",
-    <>
-      <h1 className="font-semibold">Développeur outils de diagnostic automobile niveau 3</h1>
-      <ul className="list-disc list-inside">
-        <li> Analyse des données d'entrées et des demandes d'évolution.</li>
-        <li>Développement de l'application de diagnostic DIAGBOX</li>
-        <li>
-          Création des cahiers des charges CDCs des comptes rendus d'analyse CRAs pour les calculateurs automobiles
-          STELLANTIS.
-        </li>
-        <li>Etude des spécifications fonctionnelles et techniques détaillées.</li>
-        <li>Traitement des incidents et suivi des anomalies de différents projets remontées par les clients.</li>
-      </ul>
-    </>,
-  ],
-  [
-    "Jan 2021 - Juin 2023",
-    <>
-      <h1 className="font-semibold">Consultant support technique</h1>
-      <ul className="list-disc list-inside">
-        <li>Capgemini Engineering: Support technique des techniciens auprès de STELLANTIS (PSA, OPEL)</li>
-        <li>Motorisation DIESEL/ESSENCE et chaine de transmission.</li>
-        <li>Télématique et véhicule connecté</li>
-        <li> Réseau de bord et mise en réseau (multiplexage).</li>
-      </ul>
-    </>,
-  ],
-  [
-    "Juil 2017 – Jan 2021",
-    <>
-      <h1 className="font-semibold">Technicien en diagnostic et électronique</h1>
-      <ul className="list-disc list-inside">
-        <li>Benz: Diagnostic et remise en état des systèmes électronique réseau de bord</li>
-        <li>Mise en réseaux ( multiplexage) sécurité confort et télématique Etablissement des dossiers de garantie.</li>
-        <li>Rédaction des rapports détermination des positions de travail</li>
-        <li>Technicien qualifie (programme de formation Daimler AG en 2018)</li>
-        <li>Technicien système (programme de formation Daimler AG en 2020)</li>
-        <li>Technicien certifiée (programme de formation Daimler AG en cours)</li>
-      </ul>
-    </>,
-  ],
-  [
-    "2015 – 2017",
-    <p>
-      <span className="font-semibold">Stage en alternance:</span> Mercedes Benz "Auto-Nejma"
-    </p>,
-  ],
-  [
-    "",
-    <p>
-      <span className="font-semibold">Atelier Swift Islah:</span> Maintenance des systèmes mécanique, révision générale
-    </p>,
-  ],
-  [
-    "",
-    <p>
-      <span className="font-semibold">Renault:</span> Maintenance préventive des véhicules
-    </p>,
-  ],
-]
+export const experiences = [["2018 – 2024", <>J'ai travaillé dans une épicerie pendant les étés 2018 - 2024</>]]
 
 export const languages = (
   <ul className="list-disc list-inside">
-    <li>
-      <span className="font-semibold">Arabe</span> (maternelle), <span className="font-semibold">Français</span> (bien),{" "}
-      <span className="font-semibold">Anglais</span> (moyen),
-    </li>
+    <li className="font-semibold">Arabe (maternelle)</li>
+    <li className="font-semibold">Français (bien)</li>
+    <li className="font-semibold">Anglais (moyen)</li>
   </ul>
 )
 
 export const competencies = [
-  "Diagnostic et maintenance",
-  "Maitrise des instruments de mesure",
-  "Maitrise des outils bureautique pack Microsoft office",
+  "Connaissance de Microsoft Office",
+  "Bon communicateur et auditeur actif",
+  "Apprend vite et est indépendant, nécessite peu de supervision",
 ]
